7ebefc27e4a2d2222cc84c31330656c863806a95
Jacob Bevilacqua <jacobbev@gmail.com>
2017-01-21 14:03:45 -0800

    Resolved

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170121205832) do
+ActiveRecord::Schema.define(version: 20170121220239) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20170121205832) do
     t.string   "uid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "email"
   end
 
   add_foreign_key "memberships", "groups"
