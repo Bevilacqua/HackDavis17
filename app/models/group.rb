55f6fca6e217f1bf44738f55d9d3d62ef3319904
Jacob Bevilacqua <jacobbev@gmail.com>
2017-01-21 22:48:04 -0800

    Fix

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,9 +1,4 @@
 class Group < ApplicationRecord
-<<<<<<< Updated upstream
-  has_many :memberships
-  has_many :users, through: :memberships
-  has_many :items
-=======
   before_create :generate_join_id
   
 	has_many :memberships
@@ -15,5 +10,4 @@ class Group < ApplicationRecord
       self.join_id = SecureRandom.hex
     end while self.class.exists?(join_id: join_id)
   end
->>>>>>> Stashed changes
 end
