32f00c1b24e6084fb6a82719ef08f651ef2a7301
Jacob Bevilacqua <jacobbev@gmail.com>
2017-01-21 17:19:37 -0800

    Create new groups

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
+    @group = Group.new
   end
 
   # GET /users/new
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170121220239) do
+ActiveRecord::Schema.define(version: 20170121233341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 20170121220239) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "memberships", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "group_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["group_id"], name: "index_memberships_on_group_id", using: :btree
+    t.index ["user_id"], name: "index_memberships_on_user_id", using: :btree
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "last_name"
     t.string   "first_name"
@@ -29,4 +38,5 @@ ActiveRecord::Schema.define(version: 20170121220239) do
     t.datetime "updated_at", null: false
     t.string   "email"
   end
+
 end
