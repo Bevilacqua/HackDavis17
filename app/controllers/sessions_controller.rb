521b87c2ff201075f355015959fbc337a4e485f4
Jacob Bevilacqua <jacobbev@gmail.com>
2017-01-21 23:35:25 -0800

    Started User Page
    Need to merge for correct styling

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,12 +9,12 @@ class SessionsController < ApplicationController
 	      puts "logging in user" + user.id.to_s
 
 	      if request.url.to_s.include?(root_path)
-	      	redirect_to current_user
+	      	redirect_to current_user, notice: "You're in!"
 	      else
-    	  	redirect_back(fallback_location: current_user, subdomain: false)
+    	  	redirect_back(fallback_location: current_user, subdomain: false, notice: "You're in!")
     	  end
 	    else
-	      redirect_to login_path(subdomain: false)
+	      redirect_to login_path(subdomain: false, notice: "You're in!")
 	    end
 	end
 
