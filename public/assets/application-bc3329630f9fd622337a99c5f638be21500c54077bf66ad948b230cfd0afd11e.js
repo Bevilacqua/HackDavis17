5f4478f8972ffde47965ce45b062743e890c6202
Jacob Bevilacqua <jacobbev@gmail.com>
2017-01-22 05:37:26 -0800

    assets

--- public/assets/application-bc3329630f9fd622337a99c5f638be21500c54077bf66ad948b230cfd0afd11e.js
+++ public/assets/application-bc3329630f9fd622337a99c5f638be21500c54077bf66ad948b230cfd0afd11e.js
@@ -33317,6 +33317,20 @@ var ChatRoom = React.createClass({
         this.setState({ message_val: event.target.value });
     },
 
+    handleKeyPress: function (event) {
+        if (event.key === 'Enter') {
+            event.preventDefault(); //when enter is clicked doesn't reoload page
+            $.post(this.props.items_path + ".json", {
+                title: this.props.user.first_name + " " + this.props.user.last_name,
+                body: this.state.message_val,
+                group_id: this.props.group_id
+            }).done((function () {
+                this.setState({ message_val: "" });
+                this.fetchItems();
+            }).bind(this));
+        }
+    },
+
     handleSend: function (event) {
         event.preventDefault(); //when enter is clicked doesn't reoload page
         $.post(this.props.items_path + ".json", {
@@ -33365,7 +33379,7 @@ var ChatRoom = React.createClass({
             React.createElement(
                 "div",
                 { id: "messageBar" },
-                React.createElement("input", { type: "text", className: "form-control", id: "message_bar_input", placeholder: "Hello World", onChange: this.handleMessageChange, value: this.state.message_val }),
+                React.createElement("input", { type: "text", className: "form-control", id: "message_bar_input", placeholder: "Hello World", onChange: this.handleMessageChange, value: this.state.message_val, onKeyPress: this.handleKeyPress }),
                 React.createElement(
                     "button",
                     { className: "btn btn-primary", id: "message_bar_button", onClick: this.handleSend },
